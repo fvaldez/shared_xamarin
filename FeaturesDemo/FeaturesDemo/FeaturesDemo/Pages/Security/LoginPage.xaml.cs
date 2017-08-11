e6c54afff88fce937e19576dd2f8407830dc8743
Enrique A. Garcia <enrique.garcia@ipointsystems.com>
2017-08-11 12:51:23 -0500

    Add tablet and phone UI adjustment in Login page.

--- FeaturesDemo/FeaturesDemo/FeaturesDemo/Pages/Security/LoginPage.xaml.cs
+++ FeaturesDemo/FeaturesDemo/FeaturesDemo/Pages/Security/LoginPage.xaml.cs
@@ -11,6 +11,14 @@ namespace FeaturesDemo.Pages.Security
         public LoginPage()
         {
             InitializeComponent();
+            DefineLayout();
+
+            SizeChanged += OnSizeChanged;
+        }
+
+        private void OnSizeChanged(object sender, EventArgs e)
+        {
+            DefineLayout();
         }
 
         private void btnLogin_Clicked(object sender, EventArgs e)
@@ -34,6 +42,28 @@ namespace FeaturesDemo.Pages.Security
         private async void btnCreateAccount_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Pages.Security.CreateAccountPage());
+        }
+
+        private void DefineLayout()
+        {
+            if (Device.Idiom == TargetIdiom.Tablet)
+            {
+                if (Width > Height)
+                {
+                    grdMainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
+                }
+
+                else
+                {
+                    grdMainGrid.ColumnDefinitions[1].Width = new GridLength(2, GridUnitType.Star);
+                }
+            }
+
+            else if (Device.Idiom == TargetIdiom.Phone)
+            {
+                grdMainGrid.ColumnDefinitions[0].Width = new GridLength(20);
+                grdMainGrid.ColumnDefinitions[2].Width = new GridLength(20);
+            }
         }
     }
 }
